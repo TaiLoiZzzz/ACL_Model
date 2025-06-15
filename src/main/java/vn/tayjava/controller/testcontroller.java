bdae75f5d90bf2642d089606a8cc7e3711a180e3
QTL1606 <tailoi1606@gmail.com>
2025-06-16 01:11:03 +0700

    firstcommit

---
+++ src/main/java/vn/tayjava/controller/testcontroller.java
@@ -0,0 +1,16 @@
+package vn.tayjava.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class testcontroller {
+
+    @GetMapping("test")
+    public String test() {
+        return "test";
+    }
+
+}
